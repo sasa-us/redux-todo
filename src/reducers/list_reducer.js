a58a452aeef2e7dec1130fb0fc622bd392c543c0
sasa-us <sasa_luu@hotmail.com>
2018-10-28 10:15:48 -0700

    pulled todo items from server and displayed them on the DOM

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -6,6 +6,13 @@ const DEFAULT_STATE = {
 
 export default (state=DEFAULT_STATE, action) => {
     switch(action.type) {
+        case types.GET_LIST_DATA:
+            console.log('GET_LIST_DATA',action);
+            // return state;  
+            //after get data from Promise, return new obj with new item in payload
+            return {...state, 
+                    all: action.payload.data.todos}; //set all property in state
+                    //new array data in Promise
         default: 
             return state;
     }
